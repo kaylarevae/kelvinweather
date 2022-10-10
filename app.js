1868d6721aa698bef33297db388d859cdfae4896
kaylarevae <kaylarevae@gmail.com>
2022-10-10 14:34:21 -0500

    Covert celsius to Newton

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var fahrenheit = celsius * (9 / 5) + 32;
 fahrenheit = Math.floor(fahrenheit);
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
+var newton = celsius * (33 / 100);
+newton = Math.floor(newton)
+console.log(`The temperature in Newton is ${newton}.`)
 
 
 
